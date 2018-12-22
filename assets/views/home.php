a379581fa28415e5db3b49604c6c2605ea1e4d3f
Suraj Selladurai <surajselladurai@gmail.com>
2018-12-22 13:52:52 +0530

    Privacy Update

--- assets/views/home.php
+++ assets/views/home.php
@@ -5,8 +5,8 @@
                 <tr ng-repeat="entry in $data">
                     <td data-title="'Age'" data-filter="{ age: 'assets/views/ageRange.html'}" data-sortable="'age'">{{entry.age}}</td>
                     <td data-title="'Gender'" data-filter="{ sex: 'select'}" data-filter-data='genders' data-sortable="'sex'">{{entry.sex}}</td>
-                    <td data-title="'Complaint(s)'" data-filter="{ comp: 'text'}" data-sortable="'comp'">{{entry.comp}}</td>
-                    <td data-title="'Finding(s)'" data-filter="{ find: 'text'}" data-sortable="'find'">{{entry.find}}</td>
+                    <td data-title="'Complaints'" data-filter="{ comp: 'text'}" data-sortable="'comp'">{{entry.comp}}</td>
+                    <td data-title="'Findings'" data-filter="{ find: 'text'}" data-sortable="'find'">{{entry.find}}</td>
                     <td data-title="'Match'" data-filter="{ match: 'select'}" data-filter-data='matches' data-sortable="'match'">{{entry.match}}</td>
                 </tr>
             </tbody>
